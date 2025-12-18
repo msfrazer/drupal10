3fb9c0dc92d1933dc31ee0d073954bceef2eb979
msfrazer <southerlandfrazerm@gmail.com>
2025-12-18 02:49:12 -0500

    added styling code

--- web/themes/custom/custom_bootstrap_sass/gulpfile.js
+++ web/themes/custom/custom_bootstrap_sass/gulpfile.js
@@ -47,7 +47,7 @@ const paths = {
 // Compile sass into CSS & auto-inject into browsers
 function styles() {
   return gulp
-    .src([paths.scss.bootstrap, paths.scss.src])
+    .src([paths.scss.src])
     .pipe(sourcemaps.init())
     .pipe(
       sass({
@@ -83,38 +83,7 @@ function styles() {
     .pipe(browserSync.stream());
 }
 
-function createCssComponent(fileWithLocation) {
-  return gulp
-    .src([paths.scss.components])
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({
-        outputStyle: 'compressed',
-      }).on('error', sass.logError)
-    )
-    .pipe($.postcss(postcssProcessors))
-    .pipe(
-      postcss([
-        autoprefixer({
-          browsers: [
-            'Chrome >= 35',
-            'Firefox >= 38',
-            'Edge >= 12',
-            'Explorer >= 10',
-            'iOS >= 8',
-            'Safari >= 8',
-            'Android 2.3',
-            'Android >= 4',
-            'Opera >= 12',
-          ],
-        }),
-      ])
-    )
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./components/' + '.'))
-    .pipe(cleanCss())
-    .pipe(browserSync.stream());
-}
+
 
 // Move the javascript files into our js folder
 function js() {
@@ -133,7 +102,6 @@ function serve() {
   gulp
     .watch([paths.scss.watch, paths.scss.bootstrap], styles)
     .on('change', browserSync.reload);
-  gulp.watch(paths.scss.componentsWatch, createCssComponent);
 }
 
 const build = gulp.series(styles, gulp.parallel(js, serve));
